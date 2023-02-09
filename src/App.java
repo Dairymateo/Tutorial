9d139a764ce5f0b7b7fd7bd4253c49e4d8ab8253
dairymateo <seoxyt15@gmail.com>
2023-02-09 18:03:51 -0500

    sout mas sencillo llamar a la funsion

--- src/App.java
+++ src/App.java
@@ -2,5 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("Mi nombre es Mateo");
+        System.out.println("UN gusto poder conocerte");
     }
 }
