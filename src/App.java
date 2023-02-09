b43fe3f3cec4ecc6acb7c3fb1a25fc94b6a93f59
dairymateo <seoxyt15@gmail.com>
2023-02-09 18:07:35 -0500

    los comentarios son iguales que en c

--- src/App.java
+++ src/App.java
@@ -2,6 +2,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("Mi nombre es Mateo");
-        System.out.println("UN gusto poder conocerte");
+        System.out.println("UN gusto poder conocerte");// se puede poner soaut y asi lo hace mas rapido
+        /*Asi de puede comentar todo el codigo xd*/
     }
 }
