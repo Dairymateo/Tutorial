7cb33f1e4faa20b24038b62f62cd9a59c9a059f7
dairymateo <seoxyt15@gmail.com>
2023-02-09 18:08:17 -0500

    comentario de multiple linea

--- src/App.java
+++ src/App.java
@@ -3,6 +3,7 @@ public class App {
         System.out.println("Hello, World!");
         System.out.println("Mi nombre es Mateo");
         System.out.println("UN gusto poder conocerte");// se puede poner soaut y asi lo hace mas rapido
-        /*Asi de puede comentar todo el codigo xd*/
+        /*Asi de puede comentar todo el codigo xd
+          eset comentario es de multimple linea*/
     }
 }
