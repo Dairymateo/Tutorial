f330fbb4fe1dc400aebd7b5037fec35dd88510f7
dairymateo <seoxyt15@gmail.com>
2023-02-10 14:34:33 -0500

    introduccion a variables

--- src/App.java
+++ src/App.java
@@ -2,8 +2,20 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("Mi nombre es Mateo");
-        System.out.println("UN gusto poder conocerte");// se puede poner soaut y asi lo hace mas rapido
+        System.out.println("Un gusto poder conocerte");// se puede poner soaut y asi lo hace mas rapido
         /*Asi de puede comentar todo el codigo xd
           eset comentario es de multimple linea*/
+        //variables 
+        //enteros 
+        byte entero = 12;//byte de -128 a 127
+        System.out.println("numero entero:" +entero);
+
+        short entero2 = 12456;// -32768 a 32767
+        System.out.println("numero entero: " +entero2);
+
+        
+
+
+      
     }
 }
